37541a6a8d8579e186272df77fe78848a0cc9c1b
Jason van Zyl <jvanzyl@apache.org>
2007-10-06 18:06:08 +0000

    o sketch of the round trip test that has to work in order for embedding to work
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/archetypeng@582524 13f79535-47bb-0310-9956-ffa450edef68

--- archetypeng-common/src/test/java/org/apache/maven/archetype/ArchetyperTest.java
+++ archetypeng-common/src/test/java/org/apache/maven/archetype/ArchetyperTest.java
@@ -10,5 +10,12 @@ public class ArchetyperTest
         throws Exception
     {
         Archetyper archetype = (Archetyper) lookup( Archetyper.ROLE );
+
+        // (1) create a project from scratch
+        // (2) create an archetype from the project
+        // (3) create our own archetype catalog properties in memory
+        // (4) create our own archetype catalog describing the archetype we just created
+        // (5) deploy the archetype we just created         
+        // (6) create a project form the archetype we just created
     }
 }
