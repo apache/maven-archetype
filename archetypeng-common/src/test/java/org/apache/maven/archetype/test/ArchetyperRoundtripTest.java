53a8d65fe60d3d99e579580adec154dbd3814c3d
Jason van Zyl <jvanzyl@apache.org>
2007-10-07 08:22:38 +0000

    o just the last leg to go for generating a project from the archetype we just created. there are some snags   with the environment setup but i'll finish this and then tackle those problems.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/archetypeng@582596 13f79535-47bb-0310-9956-ffa450edef68

--- archetypeng-common/src/test/java/org/apache/maven/archetype/test/ArchetyperRoundtripTest.java
+++ archetypeng-common/src/test/java/org/apache/maven/archetype/test/ArchetyperRoundtripTest.java
@@ -3,6 +3,8 @@ package org.apache.maven.archetype.test;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.archetype.ArchetypeCreationRequest;
 import org.apache.maven.archetype.ArchetypeCreationResult;
+import org.apache.maven.archetype.ArchetypeGenerationRequest;
+import org.apache.maven.archetype.ArchetypeGenerationResult;
 import org.apache.maven.archetype.Archetyper;
 import org.apache.maven.archetype.catalog.Archetype;
 import org.apache.maven.archetype.catalog.ArchetypeCatalog;
@@ -19,7 +21,9 @@ import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.cli.CommandLineUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.OutputStream;
 import java.io.Writer;
 import java.util.Arrays;
 import java.util.Properties;
@@ -85,15 +89,21 @@ public class ArchetyperRoundtripTest
 
         File catalogDirectory = new File( getBasedir(), "target/catalog" );
 
+        File catalogFile = new File( catalogDirectory, "archetype-catalog.xml" );
+
+        File catalogProperties = new File( catalogDirectory, "archetype-catalog.properties" );
+
         catalogDirectory.mkdirs();
 
         Properties p = new Properties();
 
         p.setProperty( "sources", "catalog" );
 
-        File catalogFile = new File( catalogDirectory, "archetype-catalog.xml" );
+        p.setProperty( "catalog.file", catalogFile.getAbsolutePath() );
 
-        p.setProperty( "file", catalogFile.getAbsolutePath() );
+        OutputStream os = new FileOutputStream( catalogProperties );
+
+        p.store( os, "Generated catalog properties" );
 
         // (4) create our own archetype catalog describing the archetype we just created
 
@@ -119,19 +129,19 @@ public class ArchetyperRoundtripTest
 
         // (5) install the archetype we just created
 
-        /*
-
         File generatedArchetypeDirectory = new File( project.getBasedir(), "target/generated-sources/archetypeng" );
 
         installArchetype( generatedArchetypeDirectory, new File( localRepository.getBasedir() ) );
 
         // (6) create a project form the archetype we just created
 
+        /*
         ArchetypeGenerationRequest agr = new ArchetypeGenerationRequest();
 
         ArchetypeGenerationResult generationResult = archetype.generateProjectFromArchetype( agr );
 
         */
+
     }
 
     private void installArchetype( File basedir,
@@ -144,8 +154,8 @@ public class ArchetyperRoundtripTest
         {
             InvocationRequest request = new DefaultInvocationRequest()
                 .setBaseDirectory( basedir )
-                .setGoals( Arrays.asList( new String[]{"install"} ) )
-                .setLocalRepositoryDirectory( localRepository );
+                .setGoals( Arrays.asList( new String[]{"install"} ) );
+                //.setLocalRepositoryDirectory( localRepository );
 
             Properties envars = CommandLineUtils.getSystemEnvVars();
 
