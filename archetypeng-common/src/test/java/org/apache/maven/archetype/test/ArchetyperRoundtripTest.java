5ddd04a1cacd16ac1875a7837a28130aa92f27c6
Jason van Zyl <jvanzyl@apache.org>
2007-10-07 08:14:07 +0000

    o more work on the round trip, we have a few problems in that the invoker won't just use the mvn in the path and so   you have to grab an envor which sucks as most people won't have it set. i also need to make a repo that has all the archetypeng   artifacts and run the test against that. almost there.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/archetypeng@582594 13f79535-47bb-0310-9956-ffa450edef68

--- archetypeng-common/src/test/java/org/apache/maven/archetype/test/ArchetyperRoundtripTest.java
+++ archetypeng-common/src/test/java/org/apache/maven/archetype/test/ArchetyperRoundtripTest.java
@@ -1,16 +1,28 @@
 package org.apache.maven.archetype.test;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.archetype.ArchetypeCreationRequest;
 import org.apache.maven.archetype.ArchetypeCreationResult;
 import org.apache.maven.archetype.Archetyper;
+import org.apache.maven.archetype.catalog.Archetype;
+import org.apache.maven.archetype.catalog.ArchetypeCatalog;
+import org.apache.maven.archetype.catalog.io.xpp3.ArchetypeCatalogXpp3Writer;
 import org.apache.maven.archetype.common.ArchetypeRegistryManager;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
 import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Properties;
 
 /** @author Jason van Zyl */
 public class ArchetyperRoundtripTest
@@ -26,7 +38,7 @@ public class ArchetyperRoundtripTest
         MavenProjectBuilder projectBuilder = (MavenProjectBuilder) lookup( MavenProjectBuilder.ROLE );
 
         ArtifactRepository localRepository = registryManager.createRepository(
-            new File( getBasedir(), "target/local-repo" ).toURI().toURL().toExternalForm(), "local-repo" );        
+            new File( getBasedir(), "target/local-repo" ).toURI().toURL().toExternalForm(), "local-repo" );
 
         // (1) create a project from scratch
         // (2) create an archetype from the project
@@ -60,18 +72,86 @@ public class ArchetyperRoundtripTest
 
         ArchetypeCreationRequest acr = new ArchetypeCreationRequest()
             .setProject( project )
-            .setLocalRepository( localRepository );                    
+            .setLocalRepository( localRepository );
 
-        ArchetypeCreationResult result = archetype.createArchetypeFromProject( acr );
+        ArchetypeCreationResult creationResult = archetype.createArchetypeFromProject( acr );
 
-        if ( result.getCause() != null )
+        if ( creationResult.getCause() != null )
         {
-            fail( result.getCause().getMessage() );
+            fail( creationResult.getCause().getMessage() );
         }
 
         // (3) create our own archetype catalog properties in memory
+
+        File catalogDirectory = new File( getBasedir(), "target/catalog" );
+
+        catalogDirectory.mkdirs();
+
+        Properties p = new Properties();
+
+        p.setProperty( "sources", "catalog" );
+
+        File catalogFile = new File( catalogDirectory, "archetype-catalog.xml" );
+
+        p.setProperty( "file", catalogFile.getAbsolutePath() );
+
         // (4) create our own archetype catalog describing the archetype we just created
-        // (5) deploy the archetype we just created
+
+        ArchetypeCatalog catalog = new ArchetypeCatalog();
+
+        Archetype generatedArchetype = new Archetype();
+
+        generatedArchetype.setGroupId( project.getGroupId() );
+
+        generatedArchetype.setArtifactId( project.getArtifactId() );
+
+        generatedArchetype.setVersion( project.getVersion() );
+
+        catalog.addArchetype( generatedArchetype );
+
+        ArchetypeCatalogXpp3Writer catalogWriter = new ArchetypeCatalogXpp3Writer();
+
+        Writer writer = new FileWriter( catalogFile );
+
+        catalogWriter.write( writer, catalog );
+
+        IOUtils.closeQuietly( writer );
+
+        // (5) install the archetype we just created
+
+        /*
+
+        File generatedArchetypeDirectory = new File( project.getBasedir(), "target/generated-sources/archetypeng" );
+
+        installArchetype( generatedArchetypeDirectory, new File( localRepository.getBasedir() ) );
+
         // (6) create a project form the archetype we just created
-    }       
+
+        ArchetypeGenerationRequest agr = new ArchetypeGenerationRequest();
+
+        ArchetypeGenerationResult generationResult = archetype.generateProjectFromArchetype( agr );
+
+        */
+    }
+
+    private void installArchetype( File basedir,
+                                   File localRepository )
+        throws Exception
+    {
+        Invoker invoker = (Invoker) lookup( Invoker.ROLE );
+
+        if ( basedir.exists() )
+        {
+            InvocationRequest request = new DefaultInvocationRequest()
+                .setBaseDirectory( basedir )
+                .setGoals( Arrays.asList( new String[]{"install"} ) )
+                .setLocalRepositoryDirectory( localRepository );
+
+            Properties envars = CommandLineUtils.getSystemEnvVars();
+
+            invoker.setMavenHome( new File( envars.getProperty( "M2_HOME" ) ) );
+
+            invoker.execute( request );
+        }
+    }
 }
