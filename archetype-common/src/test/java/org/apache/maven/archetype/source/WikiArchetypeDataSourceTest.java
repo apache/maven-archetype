b26399c8685be1b81855095c70dbe96cf0399f95
Raphael Pieroni <rafale@apache.org>
2008-08-25 17:14:54 +0000

    Fixed test case due to catalog reduction
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetype/trunk@688782 13f79535-47bb-0310-9956-ffa450edef68

--- archetype-common/src/test/java/org/apache/maven/archetype/source/WikiArchetypeDataSourceTest.java
+++ archetype-common/src/test/java/org/apache/maven/archetype/source/WikiArchetypeDataSourceTest.java
@@ -17,11 +17,11 @@ public class WikiArchetypeDataSourceTest
         ArchetypeDataSource ads = new InternalCatalogArchetypeDataSource();
         List archetypes = ads.getArchetypeCatalog( p ).getArchetypes();
 
-        assertEquals( 46, archetypes.size() );
+        assertEquals( 39, archetypes.size() );
 
         Archetype a = (Archetype) lookup(Archetype.class.getName());
         archetypes=a.getInternalCatalog().getArchetypes();
 
-        assertEquals( 46, archetypes.size() );
+        assertEquals( 39, archetypes.size() );
     }
 }
