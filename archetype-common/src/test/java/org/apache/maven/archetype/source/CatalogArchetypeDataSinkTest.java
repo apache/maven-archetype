1dcb556793d616a721d4a14e88778d4cfb4258ea
Raphael Pieroni <rafale@apache.org>
2009-03-24 22:04:37 +0000

    Restored camel archetypes
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetype/trunk@758045 13f79535-47bb-0310-9956-ffa450edef68

--- archetype-common/src/test/java/org/apache/maven/archetype/source/CatalogArchetypeDataSinkTest.java
+++ archetype-common/src/test/java/org/apache/maven/archetype/source/CatalogArchetypeDataSinkTest.java
@@ -86,6 +86,6 @@ public class CatalogArchetypeDataSinkTest
 
         int catalogSize = catalog.getArchetypes().size();
 
-        assertEquals( 41, catalogSize );
+        assertEquals( 46, catalogSize );
     }
 }
