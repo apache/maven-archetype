8f2132d2c4cfb0e608166b17887ab7d20c4fb8ee
Herve Boutemy <hboutemy@apache.org>
2010-08-13 19:09:19 +0000

    fixed license header position
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetype/trunk@985324 13f79535-47bb-0310-9956-ffa450edef68

--- archetype-common/src/test/java/org/apache/maven/archetype/generator/DefaultArchetypeGeneratorTest.java
+++ archetype-common/src/test/java/org/apache/maven/archetype/generator/DefaultArchetypeGeneratorTest.java
@@ -1,3 +1,5 @@
+package org.apache.maven.archetype.generator;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package org.apache.maven.archetype.generator;
 
 import org.apache.maven.archetype.ArchetypeGenerationRequest;
 import org.apache.maven.archetype.ArchetypeGenerationResult;
