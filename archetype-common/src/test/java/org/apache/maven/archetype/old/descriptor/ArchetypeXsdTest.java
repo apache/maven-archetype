a1b54f7c3dacad5fd98e1e4389cda4d1a81530a3
Herve Boutemy <hboutemy@apache.org>
2010-11-01 09:48:23 +0000

    [ARCHETYPE-341] allow content in source or resource elements of archetype.xsd
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetype/trunk@1029586 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archetype-common/src/test/java/org/apache/maven/archetype/old/descriptor/ArchetypeXsdTest.java
@@ -0,0 +1,70 @@
+package org.apache.maven.archetype.old.descriptor;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.codehaus.plexus.PlexusTestCase;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class ArchetypeXsdTest
+    extends PlexusTestCase
+{
+    private static final String ARCHETYPE_XSD = "archetype-1.0.0.xsd";
+
+    public void testXsd()
+        throws Exception
+    {
+        File archetypeXsd = new File( getBasedir(), "/target/generated-site/xsd/" + ARCHETYPE_XSD );
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating( true );
+        factory.setNamespaceAware( true );
+        SAXParser saxParser = factory.newSAXParser();
+        saxParser.setProperty( "http://java.sun.com/xml/jaxp/properties/schemaLanguage",
+                               "http://www.w3.org/2001/XMLSchema" );
+        saxParser.setProperty( "http://java.sun.com/xml/jaxp/properties/schemaSource", archetypeXsd );
+
+        saxParser.parse( new InputSource( getClass().getResourceAsStream( "sample-archetype.xml" ) ), new Handler() );
+    }
+
+    private static class Handler
+        extends DefaultHandler
+    {
+        public void warning ( SAXParseException e )
+            throws SAXException
+        {
+            throw e;
+        }
+
+        public void error ( SAXParseException e )
+            throws SAXException
+        {
+            throw e;
+        }
+    }
+}
