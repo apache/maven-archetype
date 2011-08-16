0fa4c51a4eed1643bc8581df92c7abaa03e94285
Herve Boutemy <hboutemy@apache.org>
2011-08-16 19:36:12 +0000

    removed unused code
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetype/trunk@1158428 13f79535-47bb-0310-9956-ffa450edef68

--- maven-archetype-plugin/src/main/java/org/apache/maven/archetype/mojos/CreateProjectFromArchetypeMojo.java
+++ maven-archetype-plugin/src/main/java/org/apache/maven/archetype/mojos/CreateProjectFromArchetypeMojo.java
@@ -23,7 +23,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.maven.archetype.ArchetypeManager;
 import org.apache.maven.archetype.ArchetypeGenerationRequest;
 import org.apache.maven.archetype.ArchetypeGenerationResult;
-import org.apache.maven.archetype.generator.ArchetypeGenerator;
 import org.apache.maven.archetype.ui.generation.ArchetypeGenerationConfigurator;
 import org.apache.maven.archetype.ui.generation.ArchetypeSelector;
 import org.apache.maven.artifact.repository.ArtifactRepository;
@@ -61,10 +60,7 @@ public class CreateProjectFromArchetypeMojo
     private ArchetypeSelector selector;
 
     /** @component */
-    ArchetypeGenerationConfigurator configurator;
-
-    /** @component */
-    ArchetypeGenerator generator;
+    private ArchetypeGenerationConfigurator configurator;
 
     /** @component */
     private Invoker invoker;
