c7835664867e64956bdd08f2fa3c314dcb664784
Petar Tahchiev <paranoiabla@gmail.com>
2016-01-17 21:52:12 +0200

    Replace hard-coded values in archetype-post-generate script
    Replace hard-coded values in archetype-post-generate script with corresponding request values.

--- archetype-common/src/test/archetypes/fileset_with_postscript-1.0/META-INF/archetype-post-generate.groovy
+++ archetype-common/src/test/archetypes/fileset_with_postscript-1.0/META-INF/archetype-post-generate.groovy
@@ -2,8 +2,9 @@ println "Executing the archetype-post-generate.groovy script...";
 
 def outputDirectory = new File( request.getOutputDirectory() );
 
-// TODO: file-value and file/value/package should be calculated
-def toDelete = new File( outputDirectory, "file-value/src/main/java/file/value/package/ToDelete.java" );
+def packageFolder = request.getPackage().replaceAll( '\\.', this.'file.separator' );
+
+def toDelete = new File( outputDirectory, request.getArtifactId() + "/src/main/java/${packageFolder}/ToDelete.java" );
 
 println "Removing file: " + toDelete;
 assert toDelete.delete();
