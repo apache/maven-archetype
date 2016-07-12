51f37fbd809844df8a1a5d4b67641af6b2ff3da8
Michael Osipov <michaelo@apache.org>
2016-07-12 13:41:21 +0200

    [ARCHETYPE-503] Unit tests with archetype-post-generate.groovy fail on Windows

--- archetype-common/src/test/archetypes/fileset_with_postscript-1.0/META-INF/archetype-post-generate.groovy
+++ archetype-common/src/test/archetypes/fileset_with_postscript-1.0/META-INF/archetype-post-generate.groovy
@@ -2,7 +2,7 @@ println "Executing the archetype-post-generate.groovy script...";
 
 def outputDirectory = new File( request.getOutputDirectory() );
 
-def packageFolder = request.getPackage().replaceAll( '\\.', this.'file.separator' );
+def packageFolder = request.getPackage().replace( '.', '/' );
 
 def toDelete = new File( outputDirectory, request.getArtifactId() + "/src/main/java/${packageFolder}/ToDelete.java" );
 
