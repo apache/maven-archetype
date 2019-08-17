11edd7c11b9f0417b8c7c25b81d10d3523890fc4
tibordigana <tibordigana@apache.org>
2019-08-18 00:46:29 +0200

    'println' in Groovy instead of 'System.out.println'

--- maven-archetype-plugin/src/it/projects/build-archetype/src/test/resources/projects/basic/verify.groovy
+++ maven-archetype-plugin/src/it/projects/build-archetype/src/test/resources/projects/basic/verify.groovy
@@ -17,8 +17,8 @@
  * under the License.
  */
  
-System.out.println(" Yeah Baby it rocks!")
-System.out.println("basedir:"+basedir)
+println(' Yeah Baby it rocks!')
+println("basedir:${basedir}")
 assert new File(basedir,"reference/src/main/resources/empty-directory").exists();
 assert new File(basedir,"reference/src/main/resources/empty-directory").isDirectory();
 assert new File(basedir,"project/basic/src/main/resources/empty-directory").exists();
