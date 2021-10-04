ee5e936cb78a17b4eab4102dddcd19dcb3cae184
Olivier Lamy <olamy@apache.org>
2021-10-04 12:36:46 +1000

    fix javadoc
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- archetype-common/src/main/java/org/apache/maven/archetype/common/util/Format.java
+++ archetype-common/src/main/java/org/apache/maven/archetype/common/util/Format.java
@@ -551,7 +551,7 @@ public class Format
      * Resulting behavior.
      * </th>
      * </tr>
-     * <tr valign="top">
+     * <tr>
      * <td>
      * <i>PRESERVE (Default)</i>
      * </td>
@@ -560,7 +560,7 @@ public class Format
      * or line separators are are added or removed.
      * </td>
      * </tr>
-     * <tr valign="top">
+     * <tr>
      * <td>
      * TRIM_FULL_WHITE
      * </td>
@@ -570,7 +570,7 @@ public class Format
      * printed verbatim, whitespace and all.
      * </td>
      * </tr>
-     * <tr valign="top">
+     * <tr>
      * <td>
      * TRIM
      * </td>
@@ -579,7 +579,7 @@ public class Format
      * trimmed.
      * </td>
      * </tr>
-     * <tr valign="top">
+     * <tr>
      * <td>
      * NORMALIZE
      * </td>
