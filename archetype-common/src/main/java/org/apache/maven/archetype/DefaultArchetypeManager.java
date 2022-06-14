59c5818b7e198e5ff3ac6452f4f81fcb3b48a140
Andra Maria Puscas <102651154+ampuscas@users.noreply.github.com>
2022-06-14 02:17:37 +0200

    adding a warning message for issues when getRemoteCatalog fail (#109)
    adding a warning message for issues when getRemoteCatalog fail

--- archetype-common/src/main/java/org/apache/maven/archetype/DefaultArchetypeManager.java
+++ archetype-common/src/main/java/org/apache/maven/archetype/DefaultArchetypeManager.java
@@ -198,6 +198,7 @@ public class DefaultArchetypeManager
         }
         catch ( ArchetypeDataSourceException e )
         {
+            getLogger().warn( "failed to download from remote", e );
             return new ArchetypeCatalog();
         }
     }
