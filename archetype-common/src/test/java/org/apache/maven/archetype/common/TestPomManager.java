3b2a778c29e48b52463575aa677ed5184eb0e97d
qxo <49526356@qq.com>
2023-04-17 00:35:11 +0800

    style: License above the package statement
    https://github.com/apache/maven-archetype/pull/128#discussion_r1158708580

--- archetype-common/src/test/java/org/apache/maven/archetype/common/TestPomManager.java
+++ archetype-common/src/test/java/org/apache/maven/archetype/common/TestPomManager.java
@@ -1,5 +1,3 @@
-package org.apache.maven.archetype.common;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,8 @@ package org.apache.maven.archetype.common;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package org.apache.maven.archetype.common;
 
 import java.io.File;
 import java.net.URL;
