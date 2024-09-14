cd7e86a058ea2467bdf81678c353faea80558eea
Slawomir Jaranowski <s.jaranowski@gmail.com>
2024-09-14 13:00:41 +0200

    Resolve conflicts

--- maven-archetype-plugin/src/main/java/org/apache/maven/archetype/mojos/IntegrationTestMojo.java
+++ maven-archetype-plugin/src/main/java/org/apache/maven/archetype/mojos/IntegrationTestMojo.java
@@ -404,12 +404,11 @@ public class IntegrationTestMojo extends AbstractMojo {
             do {
                 refLine = referenceFileReader.readLine();
                 actualLine = actualFileReader.readLine();
-                if ( !Objects.equals( refLine, actualLine ) )
-                {
-                    getLog().warn( "Conflict found. Reference line :" );
-                    getLog().warn( refLine );
-                    getLog().warn( "Actual line :" );
-                    getLog().warn( actualLine );
+                if (!Objects.equals(refLine, actualLine)) {
+                    getLog().warn("Conflict found. Reference line :");
+                    getLog().warn(refLine);
+                    getLog().warn("Actual line :");
+                    getLog().warn(actualLine);
                     return false;
                 }
             } while (refLine != null || actualLine != null);
